b8a95548058b769a90a87ab598a1bd5e17aa99a3
Harsh Tiwari <harshtiwari343@email.com>
2025-02-14 10:59:53 +0530

    Added CSS

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import './Welcome.css'; // External CSS
 class Welcome extends React.Component {
 
     constructor(props) {
@@ -7,9 +7,19 @@ class Welcome extends React.Component {
     }
 
     render() {
+        let myStyle = {
+            color: 'blue',
+            fontSize: ' 40px'
+        };
+
         return (<div>
-            <div>Welcome Class Component</div>
-            <div>Props Title is {this.props.title}</div>
+            <div style={{ border: '12px solid red', backgroundColor: 'yellow' }}>
+                <div style={myStyle}>Welcome Class Component</div>
+                <div style={myStyle}>Props Title is {this.props.title}</div>
+            </div>
+            <div className="anotherDiv demo">This is another div</div>
+            <div className="anotherDiv">This is another div 2</div>
+            <div id="username">Shamim</div>
         </div>);
     }
 }
